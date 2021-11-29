491fd23bc968e933e943a59fd14dd41b7a388ae2
James Blackwell <jblack@linuxguru.net>
2021-11-28 23:35:03 -0800

    subnets belong here

--- vpc/public.tf
+++ vpc/public.tf
@@ -7,12 +7,12 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
   tags = module.naming.tags
 }
-
+/*
 resource "aws_route" "public_local" {
   route_table_id = aws_route_table.public.id
-  gateway_id = aws_internet_gateway.igw.id
   destination_cidr_block = module.naming.env.cidr_block
 }
+*/
 
 resource "aws_route" "public" {
   route_table_id = aws_route_table.public.id
