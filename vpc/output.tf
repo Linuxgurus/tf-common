ef5dbe2283fd802d933e146346518d3e808b6965
James Blackwell <jblack@linuxguru.net>
2021-11-28 23:25:54 -0800

    working on igw and routes

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,5 +1,5 @@
 
 output "vpc_id" { value = aws_vpc.vpc.id }
-output "igw" { value = aws_internet_gateway.igw }
+output "igw" { value = aws_internet_gateway.igw.id }
 output "natgw" { value = var.nat_subnet ? null : aws_nat_gateway.natgw[0] }
 
