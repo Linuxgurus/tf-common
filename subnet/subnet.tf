12b3ee698f60ec0dd13e121b8b7ee7ff685c879a
James Blackwell <jblack@linuxguru.net>
2021-11-28 22:37:18 -0800

    sub module export attempt

--- subnet/subnet.tf
+++ subnet/subnet.tf
@@ -1,7 +1,7 @@
 
 resource "aws_subnet" "subnet" {
   vpc_id     = var.vpc_id
-  cidr_block = var.cidr
+  cidr_block = var.cidr.block
   tags = module.naming.tags
   availability_zone = var.availability_zone
   
