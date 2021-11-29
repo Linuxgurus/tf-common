152a34eda167926cb455a598e988eb951d12a3ca
James Blackwell <jblack@linuxguru.net>
2021-11-29 00:22:53 -0800

    get subnets right

---
+++ vpc/private_subnets/network.tf
@@ -0,0 +1,12 @@
+resource "aws_route_table" "table" {
+  vpc_id = var.vpc_id
+  tags = module.naming.tags
+}
+
+/*
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+/*
