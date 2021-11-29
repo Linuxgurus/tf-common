21aad534b3afb615d1a8fa69a954635a4f67bbce
James Blackwell <jblack@linuxguru.net>
2021-11-28 22:49:03 -0800

    sub module export attempt

--- subnet/external.tf
+++ subnet/external.tf
@@ -2,7 +2,7 @@ locals {
   stance = var.public==false ? "private" : "public" 
   az_short = substsr(var.availability_zone,-2,-1)
   calc_name  = "${var.cloud}-${local.stance}-${local.az_short}"
-  name  = coalesce(var.name, calc_name)
+  name  = coalesce(var.name, local.calc_name)
 }
 module "naming" {
   source = "git@github.com:gbpn/terraform-common.git//naming?ref=HEAD"
