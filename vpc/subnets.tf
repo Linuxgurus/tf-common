083029642be1beaaba0267144333bedb63e0a33b
James Blackwell <jblack@linuxguru.net>
2021-11-28 23:38:56 -0800

    subnets belong here

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -1,13 +1,3 @@
-module "public_subnets" {
-  source = "../subnet"
-  for_each = module.naming.env.public_subnets
-  cloud = var.cloud
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = each.key
-  availability_zone = each.value
-  public = true
-}
-
 module "private_subnets" {
   source = "../subnet"
   for_each = module.naming.env.private_subnets
