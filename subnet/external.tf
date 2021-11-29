c0125dc87c7002672278cf51f137942a2cfaa1a4
James Blackwell <jblack@linuxguru.net>
2021-11-28 22:45:30 -0800

    sub module export attempt

--- subnet/external.tf
+++ subnet/external.tf
@@ -1,8 +1,11 @@
+locals {
+  stance = var.public==false ? "private" : "public" 
+}
 module "naming" {
   source = "git@github.com:gbpn/terraform-common.git//naming?ref=HEAD"
   cloud = var.cloud
   cluster = var.cluster
   app  = var.app
-  name = "${var.cloud}-${var.availability_zone}"
+  name = "${var.cloud}-${var.stance}-${var.availability_zone}"
 }
 
