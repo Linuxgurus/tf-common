a2151fd2da3cc2d2c70b7a9060ec8f34b2f34859
James Blackwell <jblack@linuxguru.net>
2021-11-29 19:13:35 -0800

    add logging bucket

--- vpc/logs.tf
+++ vpc/logs.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 locals {
-  acct = data.aws_caller_identity.current.account_kd
+  acct = data.aws_caller_identity.current.account_id
   bucket_name = "${module.naming.tld_pre}-logs"
 }
 
