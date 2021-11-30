4851ed11c2eb83bef6638e33acd63a473d8f6db5
James Blackwell <jblack@linuxguru.net>
2021-11-29 18:45:04 -0800

    add logging bucket

--- s3_bucket/variables.tf
+++ s3_bucket/variables.tf
@@ -3,6 +3,6 @@ variable name {}
 variable cluster { default=""}
 variable app { default=""}
 variable acl { default="private" }
-variable versioning { default= false }
-variable policy { type=string default = null }
+variable versioning { default=false }
+variable policy { type=string default=null }
 
