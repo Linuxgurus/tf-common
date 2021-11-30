a429fc61772fc70c88399d66beb03706bff63d6f
James Blackwell <jblack@linuxguru.net>
2021-11-29 18:40:54 -0800

    add logging bucket

--- vpc/logs.tf
+++ vpc/logs.tf
@@ -5,3 +5,35 @@ module  "logs" {
   name = "logs"
 }
 
+resource "aws_cloudtrail" "logs" {
+  name                          = "${module.naming.pre}-logs"
+  s3_bucket_name                = module.logs.id
+  s3_key_prefix                 = "AWS_LOGS"
+  include_global_service_events = false
+    policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailAclCheck"
+        Action    = "s3:GetBucketAcl"
+        Effect    = "Allow"
+        Principal = { "Service" = "cloudtrail.amazonaws.com" }
+        Resource  = module.garbage_bucket.arn
+      },
+      {
+        Sid       = "AWSCloudTrailWrite"
+        Action    = "s3:PutObject"
+        Effect    = "Allow"
+        Principal = { "Service" = "cloudtrail.amazonaws.com" }
+        Resource  = "${module.garbage_bucket.arn}/AWS_LOGS/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+
+      }
+    ]
+  })
+}
+
